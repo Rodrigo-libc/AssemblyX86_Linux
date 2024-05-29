777560dbe08c8f25f09c7d61836f17ab2e6959ef
Rodrigo Albuquerque <rodrigo.albuquerque.linux@gmail.com>
2024-05-28 22:35:54 -0300

    Add files via upload

---
+++ HelloII.asm
@@ -0,0 +1,19 @@
+section .data
+hello db 'Hello, World!', 0xA
+tamanho equ $ - hello 
+
+section .text
+
+global _start
+_start:
+
+mov EAX, 0x4
+mov EBX, 0x1
+mov ECX, hello
+mov EDX, tamanho
+int 0x80
+
+mov EAX, 0x1
+mov EBX, 0x0
+int 0x80
+
