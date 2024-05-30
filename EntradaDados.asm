e992eabe2af51c8f2cae32c134834b7a8f7b5d9c
Rodrigo Albuquerque <rodrigo.albuquerque.linux@gmail.com>
2024-05-30 19:39:29 -0300

    Update EntradaDados.asm

--- EntradaDados.asm
+++ EntradaDados.asm
@@ -1,10 +1,8 @@
 section .data
 prompt db "Digite algo: ", 0
 
-
 section .bss
 buffer resb 128
-
 
 section .text
 
@@ -12,27 +10,27 @@ global _start
 
 _start:
 
-;Escreve a mensagem de prompt
-mov EAX, 0x4
-mov EBX, 0x1
-mov ECX, prompt
-mov EDX, 0x14
-int 0x80
+   ;Escreve a mensagem de prompt
+    mov EAX, 0x4
+    mov EBX, 0x1
+    mov ECX, prompt
+    mov EDX, 0x14
+    int 0x80
 
-;Ler a entrada do usuário
-mov EAX, 0x3
-mov EBX, 0x0
-mov ECX, buffer
-mov EDX, 128
-int 0x80
+   ;Ler a entrada do usuário
+    mov EAX, 0x3
+    mov EBX, 0x0
+    mov ECX, buffer
+    mov EDX, 128
+    int 0x80
 
-;Escrever a mensagem do usuário
-mov EAX, 0x4
-mov EBX, 0x1
-mov ECX, buffer
-int 0x80
+   ;Escrever a mensagem do usuário
+    mov EAX, 0x4
+    mov EBX, 0x1
+    mov ECX, buffer
+    int 0x80
 
-;Final
-mov EAX, 0x1
-XOR EBX, EBX
-int 0x80
+   ;Final
+    mov EAX, 0x1
+    XOR EBX, EBX
+    int 0x80
