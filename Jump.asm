2e24d1694c0ea68c1a6a69353f03458828dc8729
Rodrigo Albuquerque <rodrigo.albuquerque.linux@gmail.com>
2024-05-30 19:41:15 -0300

    Update Jump.asm

--- Jump.asm
+++ Jump.asm
@@ -7,29 +7,28 @@ global _start
 
 _start:
 
-mov EBX, msg
-mov EAX, EBX
+    mov EBX, msg
+    mov EAX, EBX
 
 nextchar:
 
-cmp byte[EAX], 0 ; Compara o byte atual na memória apontado por EAX com 0
-jz fineshed      ; Pula pra fineshed caso seja 0
-inc EAX          ; Incrementa o próximo byte até o terminador nulo 0
-jmp nextchar     ; Pula pra nextchar enquanto não finalizar
+    cmp byte[EAX], 0 ; Compara o byte atual na memória apontado por EAX com 0
+    jz fineshed      ; Pula pra fineshed caso seja 0
+    inc EAX          ; Incrementa o próximo byte até o terminador nulo 0
+    jmp nextchar     ; Pula pra nextchar enquanto não finalizar
 
 fineshed:
 
-sub EAX, EBX  ; Subtrai o endereço inical EBX com o atual em EAX
+    sub EAX, EBX  ; Subtrai o endereço inical EBX com o atual em EAX
 	      ; após a incrementação
 	      ; O resultado é o tamanho da string 
 
+    mov EDX, EAX
+    mov ECX, msg
+    mov EBX, 0x1
+    mov EAX, 0x4
+    int 0x80
 
-mov EDX, EAX
-mov ECX, msg
-mov EBX, 0x1
-mov EAX, 0x4
-int 0x80
-
-mov EBX, 0x0
-mov EAX, 0x1
-int 0x80 
+    mov EBX, 0x0
+    mov EAX, 0x1
+    int 0x80 
